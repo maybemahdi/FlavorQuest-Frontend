f01fb9ba94b6095d0ce87101950b9179bae06a73
nafis200 <nafisahamed14@gmail.com>
2025-05-02 15:52:50 +0600

    category create done

--- src/components/pages/admin-dashboard/PostCategories/PostCategories.tsx
+++ src/components/pages/admin-dashboard/PostCategories/PostCategories.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import MyButton from "@/components/ui/MyButton/MyButton";
-
+import MyFormInput from "@/components/ui/MyForm/MyFormInput/MyFormInput";
+import MyFormWrapper from "@/components/ui/MyForm/MyFormWrapper/MyFormWrapper";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
-import MyFormWrapper from "@/components/ui/MyForm/MyFormWrapper/MyFormWrapper";
-import MyFormInput from "@/components/ui/MyForm/MyFormInput/MyFormInput";
 
 const categorySchema = z.object({
   name: z.string({ required_error: "Category name is required" }),
 });
 
-export default function PostCategories() {
+export default function CategoryForm() {
   const handleSubmit = async (data: { name: string }, reset: () => void) => {
     try {
       console.log("Category Data:", data); 
@@ -25,8 +24,13 @@ export default function PostCategories() {
   };
 
   return (
-    <div className="mt-10 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-6 bg-white p-8 rounded-lg shadow-lg">
+    <div
+      className="min-h-[calc(100vh-100px)] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://i.postimg.cc/sXSt79FS/vecteezy-glowing-smooth-gradient-website-template-banner-header-or-11550522.jpg')",
+      }}
+    >
+      <div className="max-w-md w-full space-y-6 bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
         <h2 className="text-center text-2xl font-bold text-primary">Add Category</h2>
 
         <MyFormWrapper
